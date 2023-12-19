5f76f982b8ad1f7b1c38a27ceb1b590cb899d5d6
Groove <darkgraf96@gmail.com>
2023-12-19 13:30:15 +0400

    работа со скриптами

---
+++ src/js/modules/forms.js
@@ -0,0 +1,75 @@
+import checkNumInputs from "./checkNumInputs";
+
+const forms = (state) => {
+  const form = document.querySelectorAll("form"),
+    inputs = document.querySelectorAll("input");
+
+  checkNumInputs('input[name="user_phone"]');
+
+  const message = {
+    loading: "Загрузка...",
+    success: "Спасибо! Скоро мы с вами свяжемся",
+    failure: "Что-то пошло не так...",
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector(".status").textContent = message.loading;
+
+    let res = await fetch(url, {
+      method: "POST",
+      body: data,
+    });
+
+    return await res.text();
+  };
+
+  const clearInputs = () => {
+    inputs.forEach((item) => {
+      item.value = "";
+    });
+  };
+
+  form.forEach((item) => {
+    item.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      item.appendChild(statusMessage);
+
+      const formData = new FormData(item);
+      if (item.getAttribute("data-calc") === "end") {
+        for (let key in state) {
+          formData.append(key, state[key]);
+        }
+      }
+
+      postData("assets/server.php", formData)
+        .then((res) => {
+          console.log(res);
+          statusMessage.textContent = message.success;
+        })
+        .catch(() => (statusMessage.textContent = message.failure))
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+            closeModals(".popup_engineer");
+            closeModals(".popup");
+            closeModals(".popup_calc");
+            closeModals(".popup_calc_profile");
+            closeModals(".popup_calc_end");
+          }, 2500);
+        });
+    });
+  });
+
+  function closeModals(modal) {
+    const modalSelector = document.querySelector(modal);
+
+    modalSelector.style.display = "none";
+    document.body.style.overflow = "";
+  }
+};
+
+export default forms;
